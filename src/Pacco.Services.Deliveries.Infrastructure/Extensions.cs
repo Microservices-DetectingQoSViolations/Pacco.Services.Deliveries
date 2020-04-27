a70293471377f27d9d06bcd64a046ddab2e9ef44
ficmichal <meho.fic@gmail.com>
2020-04-27 21:12:13 +0200

    Update QoS Package version. Use QoSCache.

--- src/Pacco.Services.Deliveries.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Deliveries.Infrastructure/Extensions.cs
@@ -81,6 +81,7 @@ namespace Pacco.Services.Deliveries.Infrastructure
                 .UseSwaggerDocs()
                 .UseJaeger()
                 .UseConvey()
+                .UseQoSCache()
                 .UsePublicContracts<ContractAttribute>()
                 .UseMetrics()
                 .UseRabbitMq()
